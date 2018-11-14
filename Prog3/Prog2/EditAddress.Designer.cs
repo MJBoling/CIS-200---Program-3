7da07daa5951e35028a4e97b7407792117de56d8
MJBoling <35741994+MJBoling@users.noreply.github.com>
2018-11-13 21:18:37 -0500

    Add files via upload

---
+++ Prog3/Prog2/EditAddress.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace UPVApp
+{
+    partial class EditAddress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addressCB = new System.Windows.Forms.ComboBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addressCB
+            // 
+            this.addressCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.addressCB.FormattingEnabled = true;
+            this.addressCB.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.addressCB.Location = new System.Drawing.Point(42, 12);
+            this.addressCB.Name = "addressCB";
+            this.addressCB.Size = new System.Drawing.Size(121, 21);
+            this.addressCB.TabIndex = 0;
+            this.addressCB.Validating += new System.ComponentModel.CancelEventHandler(this.comboBox1_Validating);
+            this.addressCB.Validated += new System.EventHandler(this.comboBox1_Validated);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(12, 39);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cancelButton_MouseDown);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(125, 39);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 2;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // EditAddress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 72);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addressCB);
+            this.Name = "EditAddress";
+            this.Text = "EditAddress";
+            this.Load += new System.EventHandler(this.EditAddress_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox addressCB;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Prog3/Prog2/EditAddress.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPVApp
+
+{
+    public partial class EditAddress : Form
+    {
+        private List<Address> addressList;
+        public const int MIN_ADDRESSES = 1;
+        public const int index = -1;
+
+        public EditAddress(List<Address> addresses)
+        {
+            InitializeComponent();
+            addressList = addresses;
+        }
+
+        internal int AddressCBIndex
+        {
+            get
+            {
+                return comboBox1.SelectedIndex;
+            }
+
+            set
+            {
+                if ((value >= -1) && (value < addressList.Count))
+                    comboBox1.SelectedIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException("DestinationAddressIndex", value,
+                        "Index must be valid");
+            }
+        }
+
+        private void EditAddress_Load(object sender, EventArgs e)
+        {
+            if (addressList.Count < MIN_ADDRESSES)
+            {
+                MessageBox.Show("To edit an address, there must be at least one address!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.DialogResult = DialogResult.Abort;
+            }
+            else
+            {
+                foreach (Address a in addressList)
+                {
+                    comboBox1.Items.Add(a.Name);
+                }
+            }
+        }
+
+        private void comboBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (comboBox1.SelectedIndex < 0)
+            {
+                errorProvider1.SetError(comboBox1, "You must select an address!");
+                e.Cancel = true;
+            }
+        }
+
+        private void comboBox1_Validated(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                errorProvider1.SetError(comboBox1, "");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(ValidateChildren())
+            {
+                index = comboBox1.SelectedIndex;
+                this.DialogResult = DialogResult.OK;
+                /**AddressForm addressForm = new AddressForm();
+                DialogResult result = addressForm.ShowDialog();
+             
+                
+                if (result == DialogResult.OK)
+                {
+
+                }**/
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
